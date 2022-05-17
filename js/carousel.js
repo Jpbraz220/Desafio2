7493ec79791d5dc35e5bc43a9e25c11cab8d599c
João <jpbrazm220@gmail.com>
2022-05-17 14:57:07 -0300

    carrouselpronto

--- js/carousel.js
+++ js/carousel.js
@@ -1,32 +1,77 @@
 
-
-//carousel
-
-//Array storage class
 let carouselArr = [];
 
 
-//class Carousel
 class Carousel {
-
     
-      
-    static Start(arr){
-        if(arr){
+  
+    constructor(image, title, url) {
+            this.image = image;
+            this.title = title;
+            this.url = url;
+            
+    }
 
+    static Start(arr, time){
+        if(arr) {
             if(arr.length > 0){
+                Carousel.firstTime = true;
+                Carousel.count = 0;
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
+                Carousel.Next(arr); //start
+                Carousel._interval = setInterval(function(){ Carousel.Next(arr); }, time * 1000);
             }
-            
+
         } else {
             throw "Method Start need a Array Variable.";
         }
     }
 
-    static Next(){
+    static Next(arr) {
+
+        if (this.count >= arr.length) {
+            this.count = 0;
+        }
         
-    }
+        let div_selector = document.getElementById("carousel");
+        let div_title_selector = document.getElementById("carousel-title");
+        let car_image;
+        let car_title;
+        let car_link;
+
+        if (this.firstTime === true){
+        car_image = document.createElement("img");
+        car_image.setAttribute("id", "imgcar");
+        car_title = document.createElement("a");
+        car_title.setAttribute("id", "titlecar");
+        car_link = document.createElement("a");
+        car_link.setAttribute("id", "urlcar");
+        } else {
+            car_image = document.getElementById("imgcar");
+            car_title = document.getElementById("titlecar");
+            car_link = document.getElementById("urlcar")
+        }
+        
+        car_title.setAttribute("href", arr[this.count].url);
+        car_title.textContent = arr[this.count].title;
+        
+        car_image.src = "./img/" + arr[this.count].image;
+        
+        div_selector.appendChild(car_link);
+        div_selector.appendChild(car_image);
+        div_title_selector.appendChild(car_title);
+
+       this.count++;
+
+
+       if (this.firstTime === true) {
+           this.firstTime = false;
+       }
+    } 
 };
+
+
+      
+
+       // INCREMENTAR;
